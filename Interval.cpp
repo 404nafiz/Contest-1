7a9c9068a7e2eba59c0ff5d5983dca4cf5bb29fc
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-11-01 18:22:51 +0600

    Add files via upload

---
+++ Interval.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    cin >> a;
+    
+    if (0 <= a && a <= 25.0)
+        cout << "Interval [0,25]";
+    else if (25.0 < a && a <= 50.0)
+        cout << "Interval (25,50]";
+    else if (50.0 < a && a <= 75.0)
+        cout << "Interval (50,75]";
+    else if (75.0 < a && a <= 100.0)
+        cout << "Interval (75,100]";
+    else
+        cout << "Out of Intervals";
+        
+    return 0;
+}
